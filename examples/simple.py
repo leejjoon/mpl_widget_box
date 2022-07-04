b4e7d98f0620b747a602beb74d0ab404674c4e53
Jae-Joon Lee <lee.j.joon@gmail.com>
2022-07-04 16:16:53 +0900

    simplify examples/simple.py

--- examples/simple.py
+++ examples/simple.py
@@ -2,68 +2,40 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-from mpl_widget_box.widget_box import WidgetBoxManager
+from mpl_widget_box import (widgets as W,
+                            install_widgets_simple)
 
-from mpl_widget_box.widgets import (
-    Sub,
-    Title,
-    Label,
-    Radio,
-    Dropdown,
-    HWidgets,
-    DropdownMenu,
-    HPacker,
-    Button,
-    ButtonBar,
-    CheckBox,
-    HWidgets,
-)
 
 def test1():
-
 
     fig, ax = plt.subplots(num=2, clear=True)
     ax.plot(np.random.rand(10))
 
-    wbm = WidgetBoxManager(fig)
-
     sub_widgets = [
-        Label("btn4", "-- Label --"),
-        Radio("radio2", ["Ag3", "Bc3"]),
+        W.Label("btn4", "-- Label --"),
+        W.Radio("radio2", ["Ag3", "Bc3"]),
     ]
 
     widgets = [
-        Title("title0", "My Widgets"),
-        Sub("sub1", "Sub", sub_widgets, tooltip="Sub"),
-        # Dropdown("dropdown", "Dropdown", ["123", "456"]),
-        HWidgets(
-            children=[
-                Label("label2", "dropdow"),
-                Dropdown("dropdown", "Dropdown", ["123", "456"]),
+        W.Title("title0", "My Widgets"),
+        W.Sub("sub1", "Sub", sub_widgets, tooltip="Sub"),
+        W.HWidgets(
+            [
+                W.Label("label2", "dropdow"),
+                W.Dropdown("dropdown", "Dropdown", ["123", "456"]),
             ],
             align="baseline",
         ),
-        # Label("btn3", "-- Label --"),
-        Radio("radio", ["Ag", "Bc"]),
-        # Label("btn3", "-- Label --"),
-        CheckBox("check", ["1", "2", "3"], title="Check"),
-        Button("btn1", "Click", centered=True, tooltip="Tooltip"),
-        HWidgets(children=[Button("btn2", "A"), Button("btn3", "B")]),
+        W.Radio("radio", ["Ag", "Bc"]),
+        W.CheckBox("check", ["1", "2", "3"], title="Check"),
+        W.Button("btn1", "Click", centered=True, tooltip="Tooltip"),
+        W.HWidgets([W.Button("btn2", "A"), W.Button("btn3", "B")]),
     ]
-
-    wbm.add_anchored_widget_box(
-        widgets,
-        ax,
-        loc=2,
-        # callback=cb
-    )
 
     def cb(wb, ev, status):
         print(ev, status)
 
-    wbm.set_callback(cb)
-
-    wbm.install_all()
+    wbm = install_widgets_simple(ax, widgets, cb=cb, loc=2)
 
     plt.show()
     return wbm
