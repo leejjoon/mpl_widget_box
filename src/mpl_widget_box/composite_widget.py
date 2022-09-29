146f29862ab4053dc7a89889e5ff02984e24484e
Jae-Joon Lee <lee.j.joon@gmail.com>
2022-09-30 01:56:22 +0900

    support collapsable title

--- src/mpl_widget_box/composite_widget.py
+++ src/mpl_widget_box/composite_widget.py
@@ -11,17 +11,20 @@ get_icon_fontprop = FontAwesome.get_fontprop
 
 
 class TitleCollapsable(CompositeWidgetBase):
+    EXPAND = fa_icons["caret-down"]
+    COLLAPSE = fa_icons["caret-up"]
+
     def _populate_buttons(self):
-        EXPAND = fa_icons["caret-down"]
-        COLLAPSE = fa_icons["caret-up"]
         color = "red"
 
         fontprop_solid = get_icon_fontprop(family="solid", size=10)
         self.button_expand = TextArea(
-            EXPAND, textprops=dict(fontproperties=fontprop_solid, color=color)
+            self.EXPAND, textprops=dict(fontproperties=fontprop_solid,
+                                        color=color)
         )
         self.button_collapse = TextArea(
-            COLLAPSE, textprops=dict(fontproperties=fontprop_solid, color=color)
+            self.COLLAPSE, textprops=dict(fontproperties=fontprop_solid,
+                                          color=color)
         )
 
     def __init__(self, wid, title):
@@ -55,28 +58,33 @@ class TitleCollapsable(CompositeWidgetBase):
     def is_collapsed(self):
         return len(self._widgets_orig)
 
+    def collapse_widgets(self, wbm, wc):
+        widgets = [
+            self,
+        ]
+
+        wb = wc.get_widget_box()
+        self._widgets_orig = wb.widgets_orig
+
+        self._button.get_children()[0] = self.button_expand
+
+        self._button.box.set_text(self.EXPAND)
+        wc.reinit_widget_box(wbm, widgets)
+
+    def expand_widgets(self, wbm, wc):
+        widgets = self._widgets_orig
+        self._widgets_orig = []
+
+        self._button.box.set_text(self.COLLAPSE)
+        wc.reinit_widget_box(wbm, widgets)
+
     def process_event(self, wbm: WidgetBoxManager, ev: W.WidgetBoxEvent, status: dict):
         if ev.wid == f"{self.wid}:btn":
             wc = ev.container_info["container"]
 
             if not self.is_collapsed():
-                widgets = [
-                    self,
-                ]
-
-                wb = wc.get_widget_box()
-                self._widgets_orig = wb.widgets_orig
-
-                self._button.get_children()[0] = self.button_expand
-
-                self._button.box.set_text(fa_icons["caret-down"])
-
+                self.collapse_widgets(wbm, wc)
             else:
-                widgets = self._widgets_orig
-                self._widgets_orig = []
-
-                self._button.box.set_text(fa_icons["caret-up"])
-
-            wc.reinit_widget_box(wbm, widgets)
+                self.expand_widgets(wbm, wc)
 
             return True
